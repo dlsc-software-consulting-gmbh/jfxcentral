8c2011a416e7780663bff5ac547898c2cb1a9e02
floriankirmaier <florian.kirmaier@gmail.com>
2021-07-16 18:17:48 +0200

    reverted some accidental changes

--- src/main/java/com/dlsc/jfxcentral/views/mobile/MobileAdvancedListCell.java
+++ src/main/java/com/dlsc/jfxcentral/views/mobile/MobileAdvancedListCell.java
@@ -2,43 +2,18 @@ package com.dlsc.jfxcentral.views.mobile;
 
 import com.dlsc.jfxcentral.data.model.ModelObject;
 import com.dlsc.jfxcentral.util.PageUtil;
-import com.dlsc.jfxcentral.util.Util;
 import com.dlsc.jfxcentral.views.AdvancedListCell;
-import com.jpro.webapi.WebAPI;
-import javafx.collections.ObservableList;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-
-import java.lang.reflect.Method;
+import com.jpro.web.Util;
 
 public abstract class MobileAdvancedListCell<T extends ModelObject> extends AdvancedListCell<T> {
 
     protected MobileAdvancedListCell() {
         getStyleClass().add("mobile-cell");
 
-
-
-        itemProperty().addListener((p,o,item) -> {
-            if(item != null) {
-                try {
-                    ObservableList<Node> children2 = null;
-                    if (WebAPI.isBrowser()) {
-                        Method method = Parent.class.getDeclaredMethod("getChildren");
-                        method.setAccessible(true);
-                        children2 = (ObservableList<Node>) method.invoke(this.getParent());
-                    }
-                    Util.setLink(this, PageUtil.getLink(item), "description", children2);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        setOnMouseClicked(evt -> {
+            if (evt.isStillSincePress()) {
+                Util.gotoPage(this, PageUtil.getLink(getItem()));
             }
         });
-
-
-        //setOnMouseClicked(evt -> {
-        //    if (evt.isStillSincePress()) {
-        //        Util.gotoPage(this, PageUtil.getLink(getItem()));
-        //    }
-        //});
     }
 }
